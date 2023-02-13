a30140f8fd6e5bce7b13ebf74c6615c5dc655568
PareekshitPokharel <blinkmemory@gmail.com>
2023-02-13 22:55:22 +0545

    Login authoritization

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from . models import Post
+
+# Create your views here.
+
+
+
+
+def home(request):
+    context = {
+        'title': 'Home',
+        'posts': Post.objects.all()
+    }
+    return render (request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
